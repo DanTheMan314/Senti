ff7b3ccf117650ad1b98321cc796e16fa89a97b6
ankita-m-john <ankimajohn@gmail.com>
2022-06-23 00:41:38 +0530

    m

--- background.js
+++ background.js
@@ -17,7 +17,7 @@ function get_comments(videoId){
     console.log(fetch_string);
 }
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if(/^https:\/\/www\.youtube\/.com\/watch*/.test(tab.url)){
+    if(/^https:\/\/www\.youtube\.com\/watch*/.test(tab.url)){
         chrome.tabs.insertCSS(tabId, { file: 'mystyles.css'});
         chrome.tabs.executeScript(tabId, { file: 'foreground.js'})
     }
