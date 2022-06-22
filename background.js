9aaa9bb4cd0be532e83a595265e586b0fb9e4402
ankita-m-john <ankimajohn@gmail.com>
2022-06-23 00:10:18 +0530

    background done kinda

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if()
+    if(/^https:\/\/www\.youtube\/.com\/watch*/.test(tab.url)){
+        chrome.tabs.insertCSS(tabId, { file: 'mystyles.css'});
+        chrome.tabs.executeScript(tabId, { file: 'foreground.js'})
+    }
 });
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
 });
