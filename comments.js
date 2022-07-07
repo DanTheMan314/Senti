86be370b386da9337562cbdf856c162ab084397d
ankita-m-john <ankimajohn@gmail.com>
2022-07-07 06:11:59 +0530

    o

--- comments.js
+++ comments.js
@@ -1,7 +1,30 @@
-function hello() {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.message === 'here_are_your_comments'){
+        comments = request.comments;
+        
+        interval_id = setInterval(() => {
+            current_playback_time = Math.floor(document.querySelector('.video-stream').currentTime);
+            
+            comments.forEach(comment => {
+                if(current_playback_time < comment.timestamp) {
+                   if(!queued_comments.includes(comment)) queued_comments.push(comment);
+                }               
+            });
+            queued_comments.forEach(queued_comment => {
+                if(current_playback_time == queued_comment.timestamp) {
+                    console.log(queued_comment);
+                    alert(" " +queued_comment.comment);
+                    queued_comments.splice(queued_comments.indexOf(queued_comment), 1);
+                }
+            })
+        }, 100);
+    }
+});
+
+/*function hello() {
     var  name = document.getElementById('info').value;
-    alert("Hello " +name);
+    alert("Hello " +queued_comment.comment);
  }
  
  document.getElementById('btn').addEventListener('click', hello);
- //<script type="text/javascript" src="foreground.js"></script>
+ //<script type="text/javascript" src="foreground.js"></script>*/
--- popup.js
+++ popup.js
@@ -4,3 +4,9 @@ function handleButtonClick()
         popup: "page2.html"
     });
 }
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.message === 'print_the_comments'){
+        q_comment = request.queued_comment;
+    }
+});
