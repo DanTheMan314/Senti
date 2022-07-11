9591accc463ecce014311a562601c0fdd9493ea8
ankita-m-john <ankimajohn@gmail.com>
2022-07-11 20:44:54 +0530

    Tensor check

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+var q_comment = [];
 function handleButtonClick() {
     chrome.browserAction.setPopup({
         popup: "page2.html"
@@ -6,11 +7,14 @@ function handleButtonClick() {
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.message === 'print_the_comments') {
-        var q_comment = request.printed;
+        q_comment = request.printed;
     }
     var tag = document.createElement("p");
     var text = document.createTextNode(request.printed);
     tag.appendChild(text);
     var element = document.getElementById("insert");
-    element.replaceChild(tag,element.childNodes[0])
+    element.appendChild(tag,element.childNodes[0])
+    chrome.runtime.sendMessage({message: 'analyze_the_comments', printed});
 });
+
+//document.getElementById('classify-new-text').addEventListener('submit', send);
