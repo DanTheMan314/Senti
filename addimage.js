a544148425cd9a3ccf4e2dfd49ececd6596b4883
ankita-m-john <ankimajohn@gmail.com>
2022-07-24 08:40:15 +0530

    nothing

--- addimage.js
+++ addimage.js
@@ -1,4 +1,4 @@
 var img = document.createElement("img");
-img.src = "C:\Users\online\Documents\Senti-1\img.png";
+img.src = "C:\\Users\\online\\Documents\\Senti-1\\img.png";
 var div = document.getElementById("x");
 div.appendChild(img);
