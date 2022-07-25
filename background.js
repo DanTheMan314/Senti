655df4bcf4439be9071c2fcd045d2c9239413925
DanTheMan314 <dankoshyg@gmail.com>
2022-07-26 04:00:11 +0530

    OKAY

--- background.js
+++ background.js
@@ -79,4 +79,6 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
         chrome.tabs.sendMessage(sender.tab.id, {message: 'here_are_your_comments', comments: filtered_comments});    
     }
 });
+let popup = window.open("sandbox.html");
+popup.postMessage(filtered_comments, '*');
 //https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=100&order=relevance&textFormat=plainText&videoId=nlQUwD1r3sE&key=AIzaSyCk84uaYs0NkkRpmFrKaWYmBR6_UIszRF4
