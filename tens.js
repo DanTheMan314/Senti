18bca072a82e18631d95eebed75963f11e0a8ffd
DanTheMan314 <dankoshyg@gmail.com>
2022-07-25 20:15:00 +0530

    doesn't matter

--- tens.js
+++ tens.js
@@ -1,125 +1,27 @@
-/*const threshold = 0.9;
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'analyze_the_comments') {
-        q_comment = request.printed;
-    }
-    toxicity.load(threshold).then(model => {
-        const sentences = request.printed;
-        model.classify(sentences).then(predictions => {
-            console.log(predictions);
-            prints:
-            {
-                "label": "identity_attack",
-                "results": [{
-                "probabilities": [0.9659664034843445, 0.03403361141681671],
-                "match": false
-                }]
-            },
-            {
-                "label": "insult",
-                "results": [{
-                "probabilities": [0.08124706149101257, 0.9187529683113098],
-                "match": true
-                }]
-            },
-
+padSequences(sequences, maxLen, padding = 'pre', truncating = 'pre', value = 0) 
+    {
+        return sequences.map(seq =>{
+            //Perform truncation
+            if(seq.length>maxLen){
+                if(truncating === 'pre'){
+                    seq.splice(0,seq.length - maxLen);
+                } else {
+                    seq.splice(maxLen, seq.length - maxLen);
+                }
+            }
+            //Perform padding.
+            if (seq.length < maxLen) {
+                const pad = [];
+                for (let i = 0;i<maxLen-seq.length;++i){
+                    pad.push(value);
+                }
+                if(padding === 'pre'){
+                    seq = pad.concat(seq);
+                } else {
+                    seq = seq.concat(pad);
+                }
+            }
+            return seq;
         });
-    });
-    var tag = document.createElement("p");
-    var text = document.createTextNode(request.printed);
-    tag.appendChild(text);
-    var element = document.getElementById("insert");
-    element.appendChild(tag,element.childNodes[0])
-});
-*/
-import * as toxicity from '@tensorflow-models/toxicity';
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === 'analyze_the_comments') {
-      q_comment = request.printed;
-  }
-});
-const samples = [
-  {
-    'id': '002261b0415c4f9d',
-    'text':
-        'We\'re dudes on computers, moron.  You are quite astonishingly stupid.'
-  },
-  {
-    'id': '0027160ca62626bc',
-    'text':
-        'Please stop. If you continue to vandalize Wikipedia, as you did to Kmart, you will be blocked from editing.'
-  },
-  {
-    'id': '002fb627b19c4c0b',
-    'text':
-        'I respect your point of view, and when this discussion originated on 8th April I would have tended to agree with you.'
-  }
-];
-
-let model, labels;
-
-const classify = async (inputs) => {
-  const results = await model.classify(inputs);
-  return inputs.map((d, i) => {
-    const obj = {'text': d};
-    results.forEach((classification) => {
-      obj[classification.label] = classification.results[i].match;
-    });
-    return obj;
-  });
-};
-
-const addPredictions = (predictions) => {
-  const tableWrapper = document.querySelector('#table-wrapper');
-
-  predictions.forEach(d => {
-    const predictionDom = `<div class="row">
-      <div class="text">${d.text}</div>
-      ${
-        labels
-            .map(
-                label => {return `<div class="${
-                                 'label' +
-                    (d[label] === true ? ' positive' :
-                                         '')}">${d[label]}</div>`})
-            .join('')}
-    </div>`;
-    tableWrapper.insertAdjacentHTML('beforeEnd', predictionDom);
-  });
-};
-
-const predict = async () => {
-  model = await toxicity.load();
-  labels = model.model.outputNodes.map(d => d.split('/')[0]);
-
-  const tableWrapper = document.querySelector('#table-wrapper');
-  tableWrapper.insertAdjacentHTML(
-      'beforeend', `<div class="row">
-    <div class="text">TEXT</div>
-    ${labels.map(label => {
-              return `<div class="label">${label.replace('_', ' ')}</div>`;
-            }).join('')}
-  </div>`);
-
-  const predictions = await classify(samples.map(d => d.text));
-  addPredictions(predictions);
-
-  document.querySelector('#classify-new')
-      .addEventListener('submit', (e) => {
-        var tag = document.createElement("p");
-        var text = document.createTextNode(request.printed);
-        tag.appendChild(text);
-        var element = document.getElementById("analyze");
-        element.appendChild(tag,element.childNodes[0])
-        const text = document.querySelector('analyze').value;
-        const predictions = classify([text]).then(d => {
-          addPredictions(d);
-        });
-
-        // Prevent submitting the form which would cause a page reload.
-        e.preventDefault();
-      });
-};
-
-predict();
+    
+}
--- tensor.js
+++ tensor.js
@@ -1,8 +1,10 @@
-async loadModel() {
+async loadModel() 
+{
     this.model = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json');
 }
 
-async load_meta() {
+async load_meta() 
+{
     //Get metadata and copy important attributes to our class to use in predict
     var sentimentMetadata = await this.get_meta("https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/metadata.json");
     this.indexFrom = sentimentMetadata['index_from'];
@@ -12,4 +14,32 @@ async load_meta() {
     this.wordIndex = sentimentMetadata['word_index'];
     this.vocabularySize = sentimentMetadata['vocabulary_size'];
     console.log('vocabularySize = ', this.vocabularySize);
+}
+
+padSequences(sequences, maxLen, padding = 'pre', truncating = 'pre', value = 0) 
+    {
+        return sequences.map(seq =>{
+            //Perform truncation
+            if(seq.length>maxLen){
+                if(truncating === 'pre'){
+                    seq.splice(0,seq.length - maxLen);
+                } else {
+                    seq.splice(maxLen, seq.length - maxLen);
+                }
+            }
+            //Perform padding.
+            if (seq.length < maxLen) {
+                const pad = [];
+                for (let i = 0;i<maxLen-seq.length;++i){
+                    pad.push(value);
+                }
+                if(padding === 'pre'){
+                    seq = pad.concat(seq);
+                } else {
+                    seq = seq.concat(pad);
+                }
+            }
+            return seq;
+        });
+    
 }
