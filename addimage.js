62fa0da492270b50c55e34bdf9475fdb3d902f26
DanTheMan314 <dankoshyg@gmail.com>
2022-07-25 23:32:45 +0530

    spring cleaning

--- addimage.js
+++
@@ -1,4 +0,0 @@
-var img = document.createElement("img");
-img.src = "C:\\Users\\online\\Documents\\Senti-1\\img.png";
-var div = document.getElementById("x");
-div.appendChild(img);
--- service-worker.js
+++
@@ -1,5 +0,0 @@
-try{
-    importScripts('background.js', 'jquery-3.4.1.min.js');
-} catch(e) {
-    console.log(e);
-}
